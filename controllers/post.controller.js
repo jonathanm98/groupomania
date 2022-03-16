b65a1b6c0e016f88e43d0795fb1e2ea5ae69f9fa
jonathanm98 <jonathanmenelet@hotmail.fr>
2022-03-16 06:03:13 +0100

    function createpost a verifier

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -5,7 +5,7 @@ module.exports.getAllPosts = (req, res) => {
     `SELECT * FROM posts ORDER BY 'post_createdAt' ASC`,
     function (err, data) {
       if (err) res.status(500).json(err.sqlMessage);
-      res.status(200).send(data);
+      else res.status(200).send(data);
     }
   );
 };
@@ -15,13 +15,45 @@ module.exports.getOnePost = (req, res) => {
     `SELECT * FROM posts WHERE id_post = ${req.params.id}`,
     function (err, data) {
       if (err) res.status(500).json(err.sqlMessage);
-      res.status(200).send(data);
+      else res.status(200).send(data[0]);
     }
   );
 };
 
-module.exports.createPost = (req, res) => {};
+module.exports.createPost = (req, res) => {
+if (req.file) {
+  let img = `http://localhost/images/post/${req.file.filename}`
+  db.query(`
+  INSERT INTO posts (post_user, post_content, post_img)
+  VALUES
+  (
+    ${db.escape(req.body.posterId)},
+    ${db.escape(req.body.message)},
+    ${db.escape(img)}
+  )
+  `, (err,data) => {
+    console.log(data);
+    if (err) res.status(500).json(err.sqlMessage);
+    else res.status(201).send("Post créer")
+  })
+} else {
+  db.query(`
+  INSERT INTO posts (post_user, post_content)
+  VALUES
+  (
+    ${db.escape(req.body.posterId)},
+    ${db.escape(req.body.message)}
+  )
+  `, (err,data) => {
+    console.log(data);
+    if (err) res.status(500).json(err.sqlMessage);
+    else res.status(201).send("Post créer")
+  })
+}
+};
 
-module.exports.updatePost = (req, res) => {};
 
-module.exports.deletePost = (req, res) => {};
+
+module.exports.deletePost = (req, res) => {
+
+};
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,9 +1,34 @@
 const router = require("express").Router();
 const postController = require("../controllers/post.controller");
+const auth = require("../middlewares/auth");
+const multer = require("multer");
 
-router.get("/", postController.getAllPosts);
-router.get("/:id", postController.getOnePost);
+// Fonction de multer qui envoie l'image au dossier front et la renomme de façon a eviter les conflit de fichiers
+const mimetypes = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images/post");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.replace(/[^a-zA-Z]/g, "");
+    const ext = mimetypes[file.mimetype];
+    callback(null, name + Date.now() + "." + ext);
+  },
+});
+const upload = multer({ fileSize: 2097152, storage: storage });
 
-// router.post("/", postController.createPost)
+router.get("/", auth.noEdit, postController.getAllPosts);
+router.get("/:id", auth.noEdit, postController.getOnePost);
+
+router.post(
+  "/",
+  auth.noEdit,
+  upload.single("file"),
+  postController.createPost
+);
 
 module.exports = router;
