6a3fab5795b0b04db99365da5e571c3232e1d543
jonathanm98 <jonathanmenelet@hotmail.fr>
2022-04-18 17:56:40 +0200

    Modifications header IMG

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,11 +6,13 @@ import ProfilCard from "./ProfilCard";
 const Header = () => {
   const uid = useContext(UidContext);
   return (
-    <div className="logo-container">
+    <div className="header-container">
+      <div className="logo-container">
       <NavLink to="/thread">
-        <img src="./img/icon-left-font.png" alt="" />
+        <img className="header-logo" src="./img/icon-left-font.png" alt="Aller a l'accueil" />
       </NavLink>
       {uid && <ProfilCard />}
+    </div>
     </div>
   );
 };
