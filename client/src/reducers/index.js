2bb8056e07ea7fe5fd43d9033ef0b0794e4240fe
jonathanm98 <jonathanmenelet@hotmail.fr>
2022-04-18 17:59:24 +0200

    Combined reducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
+import usersReducer from "./users.reducer";
 import postsReducer from "./posts.reducer";
 
-export default combineReducers({ userReducer, postsReducer });
+export default combineReducers({ userReducer, usersReducer, postsReducer });
