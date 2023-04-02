3995091a71720f38676dd72deab14a2642e69788
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-04-02 13:08:46 +0200

    fix files

--- reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import userReducer from "./user.reducer";
-import usersReducer from "./users.reducer";
-import postsReducer from "./posts.reducer";
-
-export default combineReducers({ userReducer, usersReducer, postsReducer });
