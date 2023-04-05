53d2296c71a0e7ce4faafdfb27803a99f8962bf2
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-04-05 15:04:13 +0000

    don't run process middleware if there is no files

--- middlewares/imgProcess.js
+++ middlewares/imgProcess.js
@@ -2,6 +2,7 @@ const sharp = require('sharp');
 const fs = require("fs")
 
 module.exports.imgProcess = async (req, res, next) => {
+    if (!req.file) next()
     const type = req.file.destination.split("/")[1]
     const size = type === "user" ? 300 : 800
     const ext = `.${req.file.mimetype.split("/")[1]}`
